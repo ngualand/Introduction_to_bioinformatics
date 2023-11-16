a24557d8be73e7de290b2dcd79012db7a228c8c6
ngualand <45167445+ngualand@users.noreply.github.com>
2023-11-16 13:27:01 +0100

    Update STARalign.sh

--- scripts/STARalign.sh
+++ scripts/STARalign.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+#Map reads to the reference genome
 STAR --genomeDir "STARindex" --readFilesCommand gunzip -c --runThreadN 1 --readFilesIn "FASTQ1 and FASTQ2" --outFileNamePrefix "FILENAMEPREFIX" --outSAMtype BAM SortedByCoordinate --twopassMode Basic --outSAMattributes All --quantMode GeneCounts
+
+#Create index for BAM files
+samtools index BAMFILE
